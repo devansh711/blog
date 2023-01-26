0f2f9494bde827160d054c39464f01886d3778ae
Devansh <devanshaggarwal711@gmail.com>
2023-01-26 18:00:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const articleRouter=require('./routes/articles')
+const Article=require('./models/article')
+const methodOverride=require('method-override')
+const createDomPurify=require('dompurify')
+const {JSDOM}=require('jsdom')
+const dompurify=createDomPurify(new JSDOM().window)
+
+
+
+
+
+
+mongoose.connect('mongodb://localhost/blog',{
+    useNewUrlParser:true,useUnifiedTopology:true
+})
+
+
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+
+ app.get('/',async (req,res)=>{
+    const articles= await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index',{articles:articles})
+})
+
+app.use('/articles',articleRouter)
+app.listen(5000)
